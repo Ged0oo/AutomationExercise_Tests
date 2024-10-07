fd184cc1a940c120c06860f4b9ee00944a8e7dc5
Mohamed Nagy <mohmmednagy@gmail.com>
2024-10-07 19:59:07 +0300

    add browser and element actions

---
+++ src/main/java/browserActions/BrowserActions.java
@@ -0,0 +1,63 @@
+package browserActions;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+
+public class BrowserActions {
+
+    private WebDriver driver;
+
+    public BrowserActions(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public BrowserActions navigateToURL(String url) {
+        driver.navigate().to(url);
+        return this;
+    }
+
+    public BrowserActions getToURL(String url) {
+        driver.get(url);
+        return this;
+    }
+
+    public String getCurrentURL() {
+        return  driver.getCurrentUrl();
+    }
+
+    public BrowserActions navigateForward() {
+        driver.navigate().forward();
+        return this;
+    }
+
+    public BrowserActions navigateBackward() {
+        driver.navigate().back();
+        return this;
+    }
+
+    public BrowserActions navigateRefresh() {
+        driver.navigate().refresh();
+        return this;
+    }
+
+    public BrowserActions addCookie(Cookie cookie) {
+        driver.manage().addCookie(cookie);
+        return this;
+    }
+
+    public BrowserActions deleteCookie(Cookie cookie) {
+        driver.manage().deleteCookie(cookie);
+        return this;
+    }
+
+    public BrowserActions deleteAllCookie() {
+        driver.manage().deleteAllCookies();
+        return this;
+    }
+
+    public BrowserActions deleteCookieNamed(String cookie) {
+        driver.manage().deleteCookieNamed(cookie);
+        return this;
+    }
+
+}
--- src/main/java/elementActions/ElementActions.java
+++ src/main/java/elementActions/ElementActions.java
@@ -2,6 +2,7 @@ package elementActions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class ElementActions {
 
@@ -34,4 +35,25 @@ public class ElementActions {
         return driver.findElement(locator).isDisplayed();
     }
 
+    public ElementActions selectByIndex(By locator, int index) {
+        new Select(driver.findElement(locator)).selectByIndex(index);
+        return  this;
+    }
+
+    public ElementActions selectByValue(By locator, String val) {
+        new Select(driver.findElement(locator)).selectByValue(val);
+        return  this;
+    }
+
+    public String getTextOf(By locator) {
+        return driver.findElement(locator).getText();
+    }
+
+    public Boolean isSelected(By locator) {
+        return driver.findElement(locator).isSelected();
+    }
+
+    public Boolean isEnabled(By locator) {
+        return driver.findElement(locator).isEnabled();
+    }
 }
